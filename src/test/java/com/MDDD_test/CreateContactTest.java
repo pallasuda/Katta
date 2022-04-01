2a3ab1f3d486567e0d71156bf31964db91c9e063
pallasuda <pallasudarshan15@gmail.com>
2022-04-01 20:52:36 +0530

    mddd aded

---
+++ src/test/java/com/MDDD_test/CreateContactTest.java
@@ -0,0 +1,12 @@
+package com.MDDD_test;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	
+	@Test
+	public void CreateContact() {
+		System.out.println("create contact");
+	}
+
+}
---
+++ src/test/java/com/MDDD_test/CreateOrgTest.java
@@ -0,0 +1,11 @@
+package com.MDDD_test;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	@Test
+	public void createOTestTest() {
+		System.out.println("test added");
+	}
+
+}
