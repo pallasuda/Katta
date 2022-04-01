ccd231df00fd32cb443a4093efa8f3089fdeaa25
pallasuda <pallasudarshan15@gmail.com>
2022-04-01 20:59:14 +0530

    new org added

---
+++ src/test/java/com/MDDD_test/CreateOraganizationTest.java
@@ -0,0 +1,12 @@
+package com.MDDD_test;
+
+import org.testng.annotations.Test;
+
+public class CreateOraganizationTest {
+	@Test
+	
+	public void createTest() {
+		System.out.println("creted");
+	}
+
+}
